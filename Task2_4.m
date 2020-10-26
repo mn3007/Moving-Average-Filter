c5919fc4c1ccefd6271addd38fdfba0730f3eb98
mn3007 <18118287+mn3007@users.noreply.github.com>
2020-10-26 07:21:11 +0000

    Update Task2_4.m

--- Task2_4.m
+++ Task2_4.m
@@ -1,32 +1,50 @@
+% store the peak period data in new array
 origdata = data.Tp;
 
+% perform moving averages and store
 avedata = mov_avg(data.Tp, 5);
 avedata21 = mov_avg(data.Tp, 21);
 avedata51 = mov_avg(data.Tp, 51);
 
+% plot the original data
 subplot(3,1,1),
 plot(origdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the moving average when M = 5
 plot(avedata,'k')
+% label y-axis
 ylabel('Peak period (s)')
+% toggle visibility of minor grid lines
 grid MINOR
+% add plot title
 title('M=5')
 
+% plot the original data
 subplot(3,1,2), 
 plot(origdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the moving average when M = 21
 plot(avedata21,'k')
+% label y-axis
 ylabel('Peak period (s)')
+% toggle visibility of minor grid lines
 grid MINOR
+% add plot title
 title('M=21')
 
+% plot the original data
 subplot(3,1,3), 
 plot(origdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the moving average when M = 51
 plot(avedata51,'k')
+%label x-axis
 xlabel('Time'); 
+% label y-axis
 ylabel('Peak period (s)')
+% toggle visibility of minor grid lines
 grid MINOR
+% add plot title
 title('M=51')
 
 % Set the remaining axes properties
