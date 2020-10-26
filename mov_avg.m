4f21212aed159952c1a74a0c379a8d825e95e89c
mn3007 <18118287+mn3007@users.noreply.github.com>
2020-10-26 07:22:20 +0000

    Update mov_avg.m

--- mov_avg.m
+++ mov_avg.m
@@ -1,16 +1,21 @@
 % % --- BEGIN FUNCTION --- %
 function[yo]= mov_avg(x,M)
+% add a new function that calculates the moving average of x
 
-%zero padding
+% create array of zeros
 padding = zeros (1, M-1);
+% pad array with zeros
 x_padded = [padding, x]; 
-
-%filter length M
+% iterate for every line of x
 for n=1:length(x)
+% initialise y(n)
  y(n)=0; 
- for k=0:M-1  
+ % iterate for every value of k from 0 to M-1
+ for k=0:M-1 
+ % calculate sum
      y(n) = y(n) + x_padded(n+k);
  end;
+ % calculate output
  yo(n)=y(n)/M;
 end;
 % --- END FUNCTION --- %
