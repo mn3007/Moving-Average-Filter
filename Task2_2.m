e6e2f95d822fe70d5c6a740e22f4dffb28e398bd
mn3007 <18118287+mn3007@users.noreply.github.com>
2020-10-26 07:20:19 +0000

    Update Task2_2.m

--- Task2_2.m
+++ Task2_2.m
@@ -1,21 +1,34 @@
+%read the data into MATLAB
 data = readbuoydata('045200603.txt');
 
-% draw plot of significant wave height against time
+% plot the significant wave height against time
 subplot(2,1,1),
 plot(data.Hs)
+% convert serial date numbers to a datetime array
 plot(datetime(data.date,'ConvertFrom','datenum'),data.Hs);
+% label the tick lines of the axis using dates
 datetick('x',6 ,'keepticks')
+% label x-axis 
 xlabel('Time'); 
+% label y-axis
 ylabel('Significant Wave Height (m)');
+% add plot title
 title('Significant Wave Height and Time');
+% toggle visibility of minor grid lines
 grid MINOR
 
-% draw plot of peak period data against time
+% plot the peak period against time
 subplot(2,1,2),
 plot(data.Tp)
+% convert serial date numbers to a datetime array
 plot(datetime(data.date,'ConvertFrom','datenum'),data.Tp);
+% label the tick lines of the axis using dates
 datetick('x',6 ,'keepticks')
+% label x-axis 
 xlabel('Time'); 
+% label y-axis 
 ylabel('Peak Period (s)');
+% add plot title
 title('Peak Wave Period and Time');
+% toggle visibility of minor grid lines
 grid MINOR
