f05521db83846e7e970152f6126453908572ab5f
mn3007 <18118287+mn3007@users.noreply.github.com>
2020-10-26 07:23:43 +0000

    Update movavg_timecorr.m

--- movavg_timecorr.m
+++ movavg_timecorr.m
@@ -1,14 +1,21 @@
 % % --- BEGIN FUNCTION --- %
 function[yo]= movavg_timecorr(x,M)
-% Assuming that M is an odd integer
+% add a new function that calculates the time-corrected moving average of x
+
+% create array of zeros
 padding = zeros (1, (M-1)/2);
+% pad array with zeros
 x_padded = [padding, x, padding];
-%filter length M
+% iterate for every line of x
 for n=1:length(x)
+% initialise y(n)
  y(n)=0; 
+ % iterate for every value of k from 0 to M-1
  for k=0:M-1
+ % calculate sum
      y(n) = y(n) + x_padded(n+k);
  end;
+ % calculate output
  yo(n)=y(n)/M;
 end;
 % --- END FUNCTION --- %
