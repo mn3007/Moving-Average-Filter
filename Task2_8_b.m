f8a4911cb55e64260d9a45898d471bf6f8529711
mn3007 <18118287+mn3007@users.noreply.github.com>
2020-10-26 07:27:20 +0000

    Update Task2_8_b.m

--- Task2_8_b.m
+++ Task2_8_b.m
@@ -1,35 +1,48 @@
+% store the significant wave height data in a new array
 orighsdata = data.Hs;
 
+% perform time corrected moving averages and store
 avehsdata = movavg_timecorr(data.Hs, 5);
 avehsdata21 = movavg_timecorr(data.Hs, 21);
 avehsdata51 = movavg_timecorr(data.Hs, 51);
 
-subplot(3,1,1), 
-plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
+% plot the original data
+subplot(3,1,1), plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the time corrected moving average when M = 5
 plot(avehsdata,'k')
+% label y-axis
 ylabel('Significant Wave Height (m)')
+% toggle visibility of minor grid lines
 grid MINOR
+% add plot title
 title('M=5')
 
-subplot(3,1,2), 
-plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
+% plot the original data
+subplot(3,1,2), plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the time corrected moving average when M = 21
 plot(avehsdata21,'k')
+% label y-axis
 ylabel('Significant Wave Height (m)')
+% toggle visibility of minor grid lines
 grid MINOR
+% add plot title
 title('M=21')
 
-subplot(3,1,3), 
-plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
+% plot the original data
+subplot(3,1,3), plot(orighsdata,'Color',[0.8 0.8 0.8],'LineWidth',2)
 hold on
+% plot the time corrected moving average when M = 51
 plot(avehsdata51,'k')
+% label x-axis
 xlabel('Time'); 
+% label y-axis
 ylabel('Significant Wave Height (m)')
+% toggle visibility of minor grid lines
 grid MINOR 
+% add plot title
 title('M=51')
-
-
 
 % Set the remaining axes properties
 set(subplot(3,1,1),'XTick',[0 250 500 750 1000 1250 1500],'XTickLabel',...
